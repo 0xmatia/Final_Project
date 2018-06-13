c5181593e8d31c5b062d8ec96738d91df92ebf71
Elad Matia <elad.matia@gmail.com>
2018-06-13 14:49:04 +0300

    more progress on location

--- BossSniffer_agent.py
+++ BossSniffer_agent.py
@@ -1,23 +1,29 @@
 from scapy.layers.inet import *
 from scapy.sendrecv import *
 from uuid import getnode as get_mac
+import requests
 import socket
 
 SERVER_PORT = 8200
 BOSS_IP = "127.0.0.1"
 NUM_OF_PACKETS = 250
+IP_API = "http://ip-api.com/json/"
 
+ip_locations = {}
 packet_list = []  # the list is being erased every NUM_OF_PACKETS times
-my_mac = ':'.join(("%012X" % get_mac())[i:i+2] for i in range(0, 12, 2))  # getting the mac of the interface:
+my_mac = ':'.join(("%012X" % get_mac())[i:i + 2] for i in range(0, 12, 2))  # getting the mac of the interface:
+
+
 # https://stackoverflow.com/questions/159137/getting-mac-address
 
 
 def main():
-    init()
+
     print(my_mac)
     while True:  # the function runs forever
         packet_list.clear()
         sniff(lfilter=sniff_filter, prn=process_packet, count=NUM_OF_PACKETS)
+        get_ip_location()
 
 
 def sniff_filter(packet):
@@ -27,7 +33,8 @@ def sniff_filter(packet):
     :return: true if the packet is valid, false otherwise
     :rtype: bool
     """
-    return ((IP in packet) and (packet[IP].version == 4)) and (TCP in packet) or (UDP in packet)
+    return (IP in packet) and (TCP in packet) or ((UDP in packet) and ((packet[ UDP].sport != 5353) or  # mDNS protocol use ipv6 and crashes the program. the only solution i could find
+                                                                       (packet[UDP].dport != 5353)))
 
 
 def process_packet(packet):
@@ -42,7 +49,6 @@ def process_packet(packet):
         p_type = packet[TCP]
     else:
         p_type = packet[UDP]
-    packet.show()
     print(str(packet[IP].src) + ":" + str(p_type.sport) + " ==> " + str(packet[IP].dst) + ":" + str(p_type.dport))
     temp_dict = {"ip": packet[IP].src, "country": ""}
     if str(packet[Ether].src).upper() == str(my_mac):  # checking if the packet from my computer
@@ -64,7 +70,25 @@ def init():
     print("The IP of the boss: " + BOSS_IP)
     print("The port: " + str(SERVER_PORT))
     print("The number of packets per cycle: " + str(NUM_OF_PACKETS))
-    print("You can modify these values in the constants area. \nStarting now.\n")
+    print("You can modify these values in the constants area. Continue?y/n")
+    choice = input()
+    if choice == "n":
+        quit()
+    print("\nStarting now.\n")
+
+
+def get_ip_location():
+    for packet in packet_list:
+        if packet["ip"] in ip_locations.keys():  # if the ip is in the dictionary, we know its location
+            packet["country"] = ip_locations[packet["ip"]]
+        else:
+            response = requests.get(IP_API+packet["ip"])
+            html = response.json()
+            print(html)
+            if "fail" not in html.values():  # if the operation succeeded
+                ip_locations[packet["ip"]] = html["country"]
+    print(ip_locations)
+    quit()
 
 
 if __name__ == '__main__':
